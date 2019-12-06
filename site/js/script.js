641cd148c4d8d37d9b63fd0608e2d1c081b8a6c6
Fespi Espino <florencia.espino@gmail.com>
2019-12-06 17:38:12 -0300

    carousel + CSS

--- site/js/script.js
+++ site/js/script.js
@@ -1,3 +1,11 @@
-$(".single-item").slick({
-	dots: true
-});
+$(document).ready(function(){
+	$('.mv-slider').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		prevArrow:"<img class='a-left control-c prev slick-prev' src='img/icons/left.png'>",
+      nextArrow:"<img class='a-right control-c next slick-next' src='img/icons/right.png'>"
+	});
+  });
