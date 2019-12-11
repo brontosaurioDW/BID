0e5ea4de85ede57a5ba7390e52febfeee36a0ffb
Fespi Espino <florencia.espino@gmail.com>
2019-12-10 21:15:09 -0300

    Content + CSS + JS

--- site/js/script.js
+++ site/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 	$('.mv-slider').slick({
-		dots: false,
+		dots: true,
 		infinite: true,
 		speed: 300,
 		slidesToShow: 1,
 		adaptiveHeight: true,
-		prevArrow:"<img class='a-left control-c prev slick-prev' src='img/icons/left.png'>",
-      nextArrow:"<img class='a-right control-c next slick-next' src='img/icons/right.png'>"
+		nextArrow:"<img class='a-right control-c next slick-next' src='img/icons/right.png'>",
+		prevArrow:"<img class='a-left control-c prev slick-prev' src='img/icons/left.png'>"
 	});
   });
