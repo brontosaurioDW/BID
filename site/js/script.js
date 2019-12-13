a4be0d8b45875b7e23a95a8e499995c92da6edef
Fespi Espino <florencia.espino@gmail.com>
2019-12-13 19:15:09 -0300

    CSS + JS animations
    almost there with responsive, just carousel missing

--- site/js/script.js
+++ site/js/script.js
@@ -9,3 +9,26 @@ $(document).ready(function(){
 		prevArrow:"<img class='a-left control-c prev slick-prev' src='img/icons/left.png'>"
 	});
   });
+
+  
+
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+
+$(document).on('click', '.js-pr-btn-banner', function (event) {
+	$('html, body').animate({
+		scrollTop: $("#pr-contact").offset().top
+    }, 500);
+});
+
+
+$('.js-menu-item > a').click(function() {
+    $('.js-menu-item').removeClass('active');
+    $(this).parent().addClass('active');
+});
